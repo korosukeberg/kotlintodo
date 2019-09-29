537be3aa221cc723f5425f14f9e52faf930bcc0c
Kotaro Uchida <korosukeberg@gmail.com>
2019-09-29 22:50:35 +0900

    integrate with vuetify.js

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,126 +1,154 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <div id="header">
-      <input type="text" id="todoinput" class="form-control" v-on:keyup.enter="add" v-model="newItemTitle" />
-      <input type="button" class="btn btn-primary" value="ADD" v-on:click="add" />
-      <input type="button" class="btn btn-warning" value="CLEAR COMPLETED" v-on:click="clearCompleted" />
-      <input type="button" class="btn btn-danger" value="CLEAR ALL" v-on:click="clearAll" />
-    </div>
+    <v-app>
 
-    <ul id="todolist" class="list-group">
-      <li class="list-group-item" v-for="item in items">
-        <label v-bind:class="{done: item.isCompleted}"> <input type="checkbox" v-model="item.isCompleted" />{{ item.title }} </label>
-      </li>
-    </ul>
-  </div>
+
+        <v-content>
+            <v-container>
+                <v-layout
+                        wrap
+                >
+                    <v-flex xs12>
+                        <v-img
+                                :src="require('./assets/logo.svg')"
+                                class="my-3"
+                                contain
+                                height="200"
+                        ></v-img>
+                    </v-flex>
+
+                    <v-flex>
+                        <v-layout>
+                            <v-text-field
+                                    label="Solo"
+                                    placeholder="Add your todo here"
+                                    solo
+                                    v-on:keyup.enter="add"
+                                    v-model="newItemTitle"
+                            >
+                                <template v-slot:append>
+                                    <v-btn class="ma-2" tile color="primary" v-on:click="add">ADD</v-btn>
+                                </template>
+                            </v-text-field>
+                        </v-layout>
+                        <v-layout justify-end>
+                            <v-btn text small color="warning" v-on:click="clearCompleted">CLEAR COMPLETED
+                            </v-btn>
+                            <v-btn text small color="error" v-on:click="clearAll">CLEAR ALL</v-btn>
+                        </v-layout>
+                    </v-flex>
+
+                    <v-flex xs12>
+                        <v-card max-width="475" class="mx-auto">
+                            <v-list subheader two-line flat>
+                                <v-list-item-group multiple>
+                                    <v-list-item v-for="item in items">
+                                        <template>
+                                            <v-list-item-action>
+                                                <v-checkbox
+                                                        v-model="item.isCompleted"
+                                                        color="primary"
+                                                ></v-checkbox>
+                                            </v-list-item-action>
+
+                                            <v-list-item-content>
+                                                <v-list-item-title
+                                                        v-bind:class="{'primary--text done': item.isCompleted}">{{
+                                                    item.title }}
+                                                </v-list-item-title>
+                                            </v-list-item-content>
+                                        </template>
+                                    </v-list-item>
+                                </v-list-item-group>
+                            </v-list>
+                        </v-card>
+                    </v-flex>
+
+                    <!-- <HelloWorld/> -->
+                </v-layout>
+            </v-container>
+        </v-content>
+    </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import axiosBase from "axios"
-const axios = axiosBase.create({
-  baseURL: "http://localhost:8081"
-})
+    import HelloWorld from './components/HelloWorld';
+    import axiosBase from "axios"
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  },
-  data: function () {
-    return {
-      newItemTitle: "",
-      items: []
-    }
-  },
-  mounted: function () {
-    this.load();
-  },
-  methods: {
-    //load todos when opened <- DONE!!!
-    //save todos when added
-    //delete todos when deleted
-    load: function () {
-      axios.get("/todo").then((response) => {
-        for (let i = 0; i < response.data.length; i++) {
-          this.items.push(response.data[i])
+    const axios = axiosBase.create({
+        baseURL: "http://localhost:8081"
+    })
+
+    export default {
+        name: 'App',
+        components: {
+            HelloWorld,
+        },
+        data: function () {
+            return {
+                newItemTitle: "",
+                items: []
+            }
+        },
+        mounted: function () {
+            this.load();
+        },
+        methods: {
+            load: function () {
+                axios.get("/todo").then((response) => {
+                    for (let i = 0; i < response.data.length; i++) {
+                        this.items.push(response.data[i])
+                    }
+                })
+            },
+            add: function () {
+                if (!this.newItemTitle) {
+                    return;
+                }
+                let newItem = {
+                    id: this.items.length ? this.items.reduce((a, b) => a.id > b.id ? a : b).id + 1 : 1,
+                    title: this.newItemTitle,
+                    isCompleted: false
+                }
+                axios.post("/todo", newItem).then(response => {
+                    this.items.push(newItem);
+                    this.newItemTitle = "";
+                }, error => {
+                    console.log(error);
+                });
+            },
+            clearCompleted: function () {
+                let completedItems = [];
+                for (let i = 0; i < this.items.length; i++) {
+                    if (this.items[i].isCompleted) {
+                        completedItems.push(this.items[i].id)
+                        this.items.splice(i--, 1);
+                    }
+                }
+                if (!completedItems.length) {
+                    return
+                }
+                axios.delete("/todo", {data: completedItems}).then(response => {
+                    }
+                    , error => {
+                        console.log(error);
+                    });
+            },
+            clearAll: function () {
+                let itemIds = this.items.map(value => {
+                    return value.id
+                })
+                axios.delete("/todo", {data: itemIds}).then(response => {
+                    this.items = [];
+                }, error => {
+                    console.log(error);
+                });
+            }
         }
-      })
-    },
-    add: function() {
-      if (!this.newItemTitle) {
-        return;
-      }
-      let newItem = {
-        id: this.items.length ? this.items.reduce((a,b)=>a.id>b.id?a:b).id+1 : 1,
-        title: this.newItemTitle,
-        isCompleted: false
-      }
-      axios.post("/todo", newItem).then(response => {
-        this.items.push(newItem);
-        this.newItemTitle = "";
-      }, error => {
-        console.log(error);
-      });
-    },
-    clearCompleted: function() {
-      let completedItems = [];
-      for (let i = 0; i < this.items.length; i++) {
-        if (this.items[i].isCompleted) {
-          completedItems.push(this.items[i].id)
-          this.items.splice(i--, 1);
-        }
-      }
-      if (!completedItems.length) {
-        return
-      }
-      axios.delete("/todo", {data: completedItems}).then(response => {
-              }
-      , error => {
-        console.log(error);
-      });
-    },
-    clearAll: function() {
-      let itemIds = this.items.map(value => {
-        return value.id
-      })
-      axios.delete("/todo", {data: itemIds}).then(response => {
-        this.items = [];
-        }, error => {
-        console.log(error);
-      });
     }
-  }
-}
 </script>
-
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  width: 600px;
-  margin: 60px auto;
-}
-h2 {
-  display: flex;
-  justify-content: center;
-}
-#header {
-  display: flex;
-  justify-content: center;
-}
-#todoinput {
-  width: 100%;
-}
-#todolist {
-  margin: 60px auto;
-}
-.done {
-  text-decoration: line-through;
-}
+    .done {
+        text-decoration: line-through;
+    }
 
 </style>
