891e5356a535761217b3314bc10f02e3038f4e33
Kotaro Uchida <korosukeberg@gmail.com>
2019-10-09 20:53:44 +0900

    add createdDate to ToDo class

--- src/main/kotlin/com/github/korosukeberg/demotodo/ToDo.kt
+++ src/main/kotlin/com/github/korosukeberg/demotodo/ToDo.kt
@@ -1,6 +1,9 @@
 package com.github.korosukeberg.demotodo
 
+import java.time.LocalDateTime
+import javax.persistence.Column
 import javax.persistence.Entity
+import javax.persistence.GeneratedValue
 import javax.persistence.Id
 import javax.persistence.Table
 
@@ -8,7 +11,10 @@ import javax.persistence.Table
 @Table(name = "todo")
 data class ToDo(
     @Id
+    @GeneratedValue
     var id: Long = -1,
     var title: String = "",
-    var completed: Boolean = false
+    var completed: Boolean = false,
+    @Column(name = "createddate")
+    var createdDate: LocalDateTime = LocalDateTime.now()
 ) {}
